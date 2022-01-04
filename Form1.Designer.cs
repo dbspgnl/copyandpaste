47462a2f8d006e4db388af29c24b92743ca834a9
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-01-04 17:56:58 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CopyAndPaste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lboxTextSave = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lboxTextSave
+            // 
+            this.lboxTextSave.FormattingEnabled = true;
+            this.lboxTextSave.ItemHeight = 12;
+            this.lboxTextSave.Location = new System.Drawing.Point(13, 13);
+            this.lboxTextSave.Name = "lboxTextSave";
+            this.lboxTextSave.Size = new System.Drawing.Size(199, 196);
+            this.lboxTextSave.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 221);
+            this.Controls.Add(this.lboxTextSave);
+            this.Name = "Form1";
+            this.Text = "copys";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lboxTextSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyAndPaste
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, keyboardHookProc callback, IntPtr hInstance, uint threadId);
+        [DllImport("user32.dll")]
+        static extern int CallNextHookEx(IntPtr idHook, int nCode, int wParam, ref keyboardHookStruct IParam);
+        [DllImport("user32.dll")]
+        static extern short GetKeyState(int nCode);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string IpFileName);
+
+        public delegate int keyboardHookProc(int code, int wParam, ref keyboardHookStruct lParam);
+
+        // keyboardHookStruct 구조체 정의
+        public struct keyboardHookStruct
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        // 정의 되어 있는 상수 값
+        const int VK_SHIFT = 0x10;
+        const int VK_CONTROL = 0x11;
+        const int VK_MENU = 0x12;
+
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x100;
+        const int WM_KEYUP = 0x101;
+        const int WM_SYSKEYDOWN = 0x104;
+        const int WM_SYSKEYUP = 0x105;
+
+        private keyboardHookProc khp;
+        IntPtr hhook = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            khp = new keyboardHookProc(hookproc);
+            hook();
+        }
+
+        private void hook()
+        {
+            IntPtr hInstance = LoadLibrary("user32");
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, khp, hInstance, 0);
+        }
+
+        public int hookproc(int code, int wParam, ref keyboardHookStruct IParam) // keyboard를 낚아챔
+        {
+            if(code >= 0)
+            {
+                Keys key = (Keys)IParam.vkCode;
+                if ((GetKeyState(VK_CONTROL) & 0x80) != 0) key |= Keys.Control;
+                if ((GetKeyState(VK_MENU) & 0x80) != 0) key |= Keys.Alt;
+                if ((GetKeyState(VK_SHIFT) & 0x80) != 0) key |= Keys.Shift;
+            
+                KeyEventArgs kea = new KeyEventArgs(key);
+                if ((wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))// && (KeyDown != null))
+                {
+                    // KeyDown(this, kea);
+                }
+                else if ((wParam == WM_KEYUP || wParam == WM_SYSKEYUP))// && (KeyUp != null))
+                {
+                    // KeyUp(this, kea);
+                }
+                if (kea.Handled) return 1;
+            
+            }
+            return CallNextHookEx(hhook, code, wParam, ref IParam);
+        }
+    }
+}
